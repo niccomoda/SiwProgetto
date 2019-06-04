9ca526db62dbe41489a9586b9a17fdb54f011807
Niccomoda <niccomoda@gmail.com>
2019-06-04 16:08:26 +0200

    Added Project Siw
    Added Project Siw
    
    Co-Authored-By: niccobiagio <niccobiagio@users.noreply.github.com>

---
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.uniroma3.BiagioniModanese.SilphSPA;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SilphSpaApplication.class);
+	}
+
+}
---
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/SilphSpaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.BiagioniModanese.SilphSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilphSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilphSpaApplication.class, args);
+	}
+
+}
