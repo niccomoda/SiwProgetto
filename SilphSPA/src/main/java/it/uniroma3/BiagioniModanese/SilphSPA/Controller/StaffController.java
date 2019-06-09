b93bb1b3ed695f8d3470cf4ee88a6848a00dc05e
Niccomoda <niccomoda@gmail.com>
2019-06-09 17:52:06 +0200

    Added Class Staff
    Co-Authored-By: niccobiagio <niccobiagio@users.noreply.github.com>

---
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Controller/StaffController.java
@@ -0,0 +1,52 @@
+package it.uniroma3.BiagioniModanese.SilphSPA.Controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.BiagioniModanese.SilphSPA.Model.Staff;
+import it.uniroma3.BiagioniModanese.SilphSPA.Service.StaffService;
+import it.uniroma3.BiagioniModanese.SilphSPA.Service.StaffValidator;
+
+@Controller
+public class StaffController {
+	
+	@Autowired
+	private StaffValidator staffValidator;
+	
+	@Autowired
+	private StaffService staffService;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@Valid @ModelAttribute("staff") Staff staff, Model model, BindingResult bindingResult) {
+		this.staffValidator.validate(staff, bindingResult);
+		if(!bindingResult.hasErrors()) {
+			Staff c = staffService.login(staff.getUser(), staff.getPassword());
+			if(c == null) {
+				bindingResult.rejectValue("username", "wrong");
+				return "login.html";
+			}
+			else {
+				return "successo.html";
+			}
+		
+		}
+		else {
+			return "login.html";
+		}
+	}
+	
+	@RequestMapping(value = "/loginFrom")
+	public String loginForm(Model model) {
+		model.addAttribute("Staff", new Staff());
+		return "login.html";
+	}
+	
+	
+}
---
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Service/StaffService.java
@@ -0,0 +1,25 @@
+package it.uniroma3.BiagioniModanese.SilphSPA.Service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.BiagioniModanese.SilphSPA.Model.Staff;
+import it.uniroma3.BiagioniModanese.SilphSPA.Repository.StaffRepository;
+
+@Service
+public class StaffService {
+	@Autowired
+	private StaffRepository staffRepository;
+	
+	@Transactional
+	public Staff login(String username, String password) {
+		return staffRepository.findByUsernameAndPassword(username, password);
+		
+	}
+	
+	
+}
