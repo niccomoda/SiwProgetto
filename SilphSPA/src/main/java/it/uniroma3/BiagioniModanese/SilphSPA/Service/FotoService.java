cba29c1abb3a26ef9c9e6c1ce179205871184024
niccobiagio <niccolobiagioni.96@hotmail.com>
2019-06-09 13:55:08 +0200

    Class FotoService
    Add new Class FotoService
    
    Co-Authored-By: niccomoda <niccomoda@users.noreply.github.com>

---
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Service/FotoService.java
@@ -0,0 +1,21 @@
+package it.uniroma3.BiagioniModanese.SilphSPA.Service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.BiagioniModanese.SilphSPA.Model.Foto;
+import it.uniroma3.BiagioniModanese.SilphSPA.Repository.FotoRepository;
+
+@Service
+public class FotoService {
+
+	@Autowired
+	private FotoRepository fotoRepository;
+	
+	@Transactional
+	public Foto inserisci(Foto foto) {
+		return fotoRepository.save(foto);
+	}
+}
