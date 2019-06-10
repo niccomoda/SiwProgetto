db0546eb339af0b1bb959b5bd04f7e863d1e8abf
Niccomoda <niccomoda@gmail.com>
2019-06-10 10:36:19 +0200

    Modified pom and Staff class

--- SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Model/Staff.java
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Model/Staff.java
@@ -1,12 +1,22 @@
 package it.uniroma3.BiagioniModanese.SilphSPA.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import com.vaadin.flow.component.polymertemplate.Id;
 
 @Entity
 public class Staff {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	private String username;
 	private String password;
+	
+	public Staff() {
+		
+	}
 	
 	public String getUser() {
 		return username;
