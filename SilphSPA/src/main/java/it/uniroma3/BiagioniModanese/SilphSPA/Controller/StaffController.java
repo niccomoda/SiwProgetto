c72756bf71c700c39e41128fdf1cfbcc329fe71f
Niccomoda <niccomoda@gmail.com>
2019-06-17 13:39:22 +0200

    new login and some pages

--- SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Controller/StaffController.java
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Controller/StaffController.java
@@ -1,12 +1,9 @@
  package it.uniroma3.BiagioniModanese.SilphSPA.Controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,7 +23,7 @@ public class StaffController {
 	@Autowired
 	private StaffService staffService;
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	/*@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(@Valid @ModelAttribute("staff") Staff staff, Model model, BindingResult bindingResult) {
 		this.staffValidator.validate(staff, bindingResult);
 		if(!bindingResult.hasErrors()) {
@@ -49,7 +46,30 @@ public class StaffController {
 	public String loginForm(Model model) {
 		model.addAttribute("staff", new Staff());
 		return "login.html";
+	}*/
+	
+	@RequestMapping(value = "/admin")
+	public String login(Model model) {
+		return "admin.html";
 	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET )
+	public String loginView(Model model) {
+
+		/*Staff ss = new Staff();
+		ss.setUsername("admin");
+		ss.setRole("admin");
+		ss.setPassword(new BCryptPasswordEncoder().encode("admin"));
+		this.staffService.inserisci(ss);*/
+		return "login";
+	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET )
+	public String logout(Model model) {
+		return "logout";
+	}
+
+	
 	
 	@RequestMapping(value = "/inserimentoFoto")
 	public String inserimentoFoto(Model model) {
@@ -67,6 +87,16 @@ public class StaffController {
 	public String inserimentoAlbum(Model model) {
 		model.addAttribute("albumForm", new AlbumForm());
 		return "inserimentoAlbum.html";
+	}
+	
+	@RequestMapping(value = "/inserimento", method = RequestMethod.GET)
+	public String inserimento(Model model) {
+		return "inserimento.html";
+	}
+	
+	@RequestMapping(value = "/consulta")
+	public String ricerca() {
+		return "ricerca.html";
 	}
 	
 	
--- SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Service/SecurityConfig.java
+++ SilphSPA/src/main/java/it/uniroma3/BiagioniModanese/SilphSPA/Service/SecurityConfig.java
@@ -1,7 +1,15 @@
 package it.uniroma3.BiagioniModanese.SilphSPA.Service;
 
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,12 +20,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	 @Autowired
+	 private Environment environment;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http
 		.authorizeRequests()
-		.antMatchers("/admin").authenticated()
-		.anyRequest().permitAll();
+		.antMatchers("/admin").hasAnyAuthority("admin")
+		.anyRequest().permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.defaultSuccessUrl("/admin")
+		.and()
+		.logout()
+		.permitAll();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		  auth.jdbcAuthentication().dataSource(this.buildDatasource())
+          .authoritiesByUsernameQuery("SELECT username,role FROM staff WHERE username=?")
+          .usersByUsernameQuery("SELECT username, password, 1 as enabled FROM staff WHERE username=?");
+
+	}
+
+	@Bean
+	DataSource buildDatasource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(
+				environment.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+		dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+		dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+		return dataSource;
+
 	}
 	
 	@Bean
